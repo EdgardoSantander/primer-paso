8dcff9abc08fa2a8193dbe3cc30b9537a749ca34
Edgardo Santander <edgarsa1209@gmail.com>
2026-03-11 16:34:00 -0600

    creacion de endpoints para registro/login

---
+++ src/main/java/com/firststep/primer_paso/controller/UsuarioController.java
@@ -0,0 +1,49 @@
+package com.firststep.primer_paso.controller;
+
+import com.firststep.primer_paso.dto.LoginDTO;
+import com.firststep.primer_paso.dto.UsuarioRegistroDTO;
+import com.firststep.primer_paso.service.RefreshTokenService;
+import com.firststep.primer_paso.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("taxi/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<Map<String, String>> registro(@RequestBody UsuarioRegistroDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.registrar(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDTO dto) {
+        return ResponseEntity.ok(usuarioService.login(dto));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Map<String,String>> refresh(@RequestBody Map<String,String> request){
+       String nuevoAccessToken = refreshTokenService.renovarAccessToken(request.get("refreshToken"));
+       return ResponseEntity.ok(Map.of("accessToken",nuevoAccessToken));
+    }
+
+}
+
+
+
+
+
+
